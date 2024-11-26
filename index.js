ef9bea9720aa9d17d750ae1e8333ea4a27afc9fb
Rayane Pimentel <Rayane.Pimentel@ibm.com>
2024-11-26 15:43:53 -0300

    (feat): atualizando index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // index.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from CI/CD Pipeline! Feature exemplo 1');
+app.get("/", (req, res) => {
+    res.send("Hello from CI/CD Pipeline! Feature exemplo 1 !!!");
 });
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
